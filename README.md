4414aaafaef88e2f63850cc1410be913880e95dd
s8435330song <s8435330song@126.com>
2019-04-22 12:14:58 +0800

    final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # netease-test
+base1
+base2
---
+++ script/tocsrtest.py
@@ -0,0 +1,7 @@
+from numpy import array
+from scipy.sparse import coo_matrix
+row  = array([0, 0, 1, 3, 1, 0, 0])
+col  = array([0, 2, 1, 3, 1, 0, 0])
+data = array([1, 1, 1, 1, 1, 1, 1])
+A = coo_matrix((data, (row, col)), shape=(4, 4)).tocsr()
+A.toarray()
